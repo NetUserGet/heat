4bb403b3fff43678f5d865c559e76d2b7de99105
NetUserGet <22157020+NetUserGet@users.noreply.github.com>
2025-04-21 22:04:55 -0400

    It keeps on trying to add .env I wont let it..

--- main.py
+++ main.py
@@ -10,7 +10,6 @@ class MyClient(discord.Client):
         print(f'Logged on as {self.user}!')
 
     async def on_message(self, message):
-
         print(f'Message from {message.author}: {message.content}')
 
 intents = discord.Intents.default()
